b4578334efe4917246c287a8c424b7fbd580ed33
shuqingle_CN@163.com <shuqingle_CN@163.com>
2022-10-13 11:35:41 +0800

    最小生成树

---
+++ jago-bottom-core-tech-stack/data-structure-algorithm-tech/algorithm-book-algorithms/algorithm-book-algorithms/src/com/jago/B394_Edge.java
@@ -0,0 +1,38 @@
+package com.jago;
+
+
+public class B394_Edge implements Comparable<B394_Edge> {
+    private final int v;
+    private final int w;
+    private final double weight;
+
+    public B394_Edge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+    public double weight(){
+        return weight;
+    }
+
+    public int either(){
+        return v;
+    }
+
+    public int other(int vertex){
+        if (vertex == v) return w;
+        else if (vertex == w) return v;
+        else throw new RuntimeException("Inconsistent edge");
+    }
+
+    @Override
+    public int compareTo(B394_Edge that) {
+        if (this.weight < that.weight) return -1;
+        else if (this.weight > that.weight) return 1;
+        else return 0;
+    }
+
+    public String toString(){
+        return String.format("%d-%d %.2f",v,w,weight);
+    }
+}
---
+++ jago-bottom-core-tech-stack/data-structure-algorithm-tech/algorithm-book-algorithms/algorithm-book-algorithms/src/com/jago/B395_EdgeWeightedGraph.java
@@ -0,0 +1,61 @@
+package com.jago;
+
+import java.util.Iterator;
+
+public class B395_EdgeWeightedGraph {
+    private final int V;
+    private int E;
+    private Bag<B394_Edge>[] adj;
+    public B395_EdgeWeightedGraph(int V){
+        this.V = V;
+        this.E = E;
+        adj = (Bag<B394_Edge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<>();
+        }
+    }
+
+    public int V(){
+        return V;
+    }
+
+    public int E(){
+        return E;
+    }
+
+    public void addEdge(B394_Edge e){
+        int v = e.either();
+        int w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+    }
+
+    public Iterable<B394_Edge> adj(int v){
+        return adj[v];
+    }
+
+    public Iterable<B394_Edge> edges(){
+        Bag<B394_Edge> b = new Bag<>();
+        for (int v = 1; v < V; v++) {
+            for(B394_Edge e:adj(v)){
+                if (e.other(v) > v){
+                    b.add(e);
+                }
+            }
+        }
+        return b;
+    }
+
+
+
+    private class  Bag<T> implements Iterable{
+
+        @Override
+        public Iterator iterator() {
+            return null;
+        }
+
+        public void add(T t){};
+
+    }
+}
