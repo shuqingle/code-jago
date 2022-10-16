e9f6eb6a8ff8517052399de6bb0b87380ec9234b
shuqingle_CN@163.com <shuqingle_CN@163.com>
2022-10-16 16:46:20 +0800

    测试方法区内存溢出代码

--- jago-bottom-core-tech-stack/jvm-core-tech-tech/jvm-vedio-heima/src/com/jago/V023_Demo1_8.java
+++ jago-bottom-core-tech-stack/jvm-core-tech-tech/jvm-vedio-heima/src/com/jago/V023_Demo1_8.java
@@ -11,11 +11,13 @@ public class V023_Demo1_8 extends ClassLoader {
         int j = 0;
         try {
             V023_Demo1_8 test = new V023_Demo1_8();
-            for (int i = 0; i < 10000; i++) {
+            for (int i = 0; i < 10000; i++,j++) {
                 //ClassWriter是用代码的方式来生成类的字节码
                 ClassWriter cw = new ClassWriter(0);
+                //参数解析：jdk1.8,public,类名，包名，父类，接口
                 cw.visit(Opcodes.V1_8,Opcodes.ACC_PUBLIC,"Class"+i,null,"java/lang/Object",null);
                 byte[] code = cw.toByteArray();
+                //使用类加载器加载触发类的加载，生成class对象
                 test.defineClass("Class"+i,code,0,code.length);
             }
         } finally {
