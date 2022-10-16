4727da7ac2d75a3791cf0ba57658dc991c4523eb
shuqingle_CN@163.com <shuqingle_CN@163.com>
2022-10-16 22:07:27 +0800

    2111添加注释

--- jago-bottom-core-tech-stack/jvm-core-tech-tech/jvm-vedio-heima/src/com/jago/V025_HelloWorld.java
+++ jago-bottom-core-tech-stack/jvm-core-tech-tech/jvm-vedio-heima/src/com/jago/V025_HelloWorld.java
@@ -2,7 +2,10 @@ package com.jago;
 
 public class V025_HelloWorld {
     public static void main(String[] args) {
-        //测试注释
+        //测试注释11111
+
+
+        
         System.out.println("hello world");
     }
 }
