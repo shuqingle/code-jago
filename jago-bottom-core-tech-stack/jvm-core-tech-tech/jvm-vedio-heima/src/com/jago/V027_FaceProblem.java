6d1139c64dc0ddfce862a5314f3dc9d1abd7d257
shuqingle_CN@163.com <shuqingle_CN@163.com>
2022-10-17 22:12:13 +0800

    面试题分析

--- jago-bottom-core-tech-stack/jvm-core-tech-tech/jvm-vedio-heima/src/com/jago/V027_FaceProblem.java
+++ jago-bottom-core-tech-stack/jvm-core-tech-tech/jvm-vedio-heima/src/com/jago/V027_FaceProblem.java
@@ -8,19 +8,25 @@ public class V027_FaceProblem {
         String s1 = "a";
         String s2 = "b";
         String s3 = "a"+"b";
-        String s4 = s1+s2;
+        String s4 = s1+s2;//new String("ab")
         String s5= "ab";
         String s6 = s4.intern();
 
-        System.out.println(s3==s4);
-        System.out.println(s3==s5);
-        System.out.println(s3==s6);
+        System.out.println(s3==s4);//false
+        System.out.println(s3==s5);//true
+        System.out.println(s3==s6);//true
 
-        String x2 = new String("c") + new String("d");
+//        String x2 = new String("c") + new String("d");
+//        String x1 = "cd";
+//        x2.intern();
+//        //问：如果调换了最后两行代码的位置呢？如果是jdk1.6呢？
+//        System.out.println(x1 == x2);//false;
+
         String x1 = "cd";
+        String x2 = new String("c") + new String("d");
         x2.intern();
         //问：如果调换了最后两行代码的位置呢？如果是jdk1.6呢？
-        System.out.println(x1 == x2);
+        System.out.println(x1 == x2);//true;
 
     }
 }
