3ad014a51fd04586e0f39c5e49da4bbce8ca1e21
jagosh <shuqingle_CN@163.com>
2022-11-14 19:45:00 +0800

    最大优先队列

--- jago-bottom-core-tech-stack/data-structure-algorithm-tech/algorithm-vedio-heima/src/com/jago/V107_MaxPriorityQueue.java
+++ jago-bottom-core-tech-stack/data-structure-algorithm-tech/algorithm-vedio-heima/src/com/jago/V107_MaxPriorityQueue.java
@@ -1,5 +1,11 @@
 package com.jago;
 
+import java.util.Queue;
+
+/**
+ * 最大优先队列
+ * @param <T>
+ */
 public class V107_MaxPriorityQueue<T extends Comparable<T>>{
     //存储堆中的元素
     private T[] items;
@@ -8,46 +14,82 @@ public class V107_MaxPriorityQueue<T extends Comparable<T>>{
 
     public V107_MaxPriorityQueue(int capacity) {
         this.items = (T[])new Comparable[capacity+1];
-        
+        this.N = 0;
     }
 
     //获取队列中元素的个数
     public int size(){
-        return -1;
+        return N;
     }
 
     //判断队列是否为空
     public boolean isEmpty(){
-        return false;
+        return N==0;
     }
 
     //判断堆中索引i处的元素是否小于索引j处的元素
     private boolean less(int i,int j){
-        return false;
+        return items[i].compareTo(items[j]) < 0;
     }
 
     //交换堆中i索引和j索引的值
     private void exch(int i,int j){
-
+        T temp = items[i];
+        items[i] = items[j];
+        items[j] = temp;
     }
 
     //往堆中插入一个元素
     public void insert(T t){
-
+        items[++N] = t;
+        swim(N);
     }
 
     //删除堆中最大的元素，并返回这个最大元素
     public T delMax(){
-        return null;
+        T max = items[1];
+        exch(1,N);
+        N--;
+        sink(1);
+        return max;
     }
 
     //使用上浮算法，使索引k处的元素能处在一个正确的位置
     private void swim(int k){
-
+        while (k > 1){
+            if (less(k/2,k)) exch(k/2,k);
+            k = k/2;
+        }
     }
     //使用下沉算法，使索引k处的元素能处在一个正确的位置
     private void sink(int k ){
+        while (2*k <= N){
+            int max;
+            if (2*k+1 <= N){
+                //有右子结点
+                if (less(2*k,2*k+1)) max = 2*k+1;
+                else max = 2*k;
+            }else {
+                max = 2*k;
+            }
+            if (!less(k,max)) break;
+            exch(k,max);
+            k = max;
+        }
+    }
 
+    public static void main(String[] args) {
+        V107_MaxPriorityQueue<String> queue = new V107_MaxPriorityQueue<>(10);
+        queue.insert("C");
+        queue.insert("B");
+        queue.insert("A");
+        queue.insert("D");
+        queue.insert("E");
+        queue.insert("G");
+        queue.insert("F");
+        while (!queue.isEmpty()){
+            System.out.print(queue.delMax()+" ");
+        }
     }
 
 }
